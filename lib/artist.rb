2928d9982064d15273084d9ee46eb7aa2dc1260f
MJ Johnson <mjohnson4685@gmail.com>
2020-09-23 16:25:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,15 +23,11 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name } || Artist.new(name)
+    self.all.detect { |artist| artist.name == name } || Artist.new(name)
   end
 
   def print_songs
-    @songs.each {|song| puts song.name}
-
+    @songs.each { |song| puts song.name }
   end
-
-
-
 
 end
