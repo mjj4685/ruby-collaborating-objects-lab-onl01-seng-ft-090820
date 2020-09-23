2c72c024862ea000dd128220cf07912231e4f86f
MJ Johnson <mjohnson4685@gmail.com>
2020-09-23 16:23:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,16 +9,16 @@ class Artist
     @songs = []
   end
 
-  def self.all
-    @@all
-  end
-
   def add_song(song)
     @songs << song
   end
 
   def save
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def self.find_or_create_by_name(name)
