a56fafc7589f20a9a2a3394937acde0f74f62b6e
MJ Johnson <mjohnson4685@gmail.com>
2020-09-23 15:47:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+   end
+
+
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+
+
+
+
+end
