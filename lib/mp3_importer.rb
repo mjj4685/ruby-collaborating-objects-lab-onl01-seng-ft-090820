8695a451519797995ee99f0d5e902f0af932c04b
MJ Johnson <mjohnson4685@gmail.com>
2020-09-23 16:30:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,22 +1,17 @@
 class MP3Importer
 
-  attr_reader :path
+  attr_accessor :path
 
-  def initialize(filepath)
-    @path = filepath
+  def initialize(path)
+    @path = path
   end
 
   def files
-    @files = Dir.entries(@path)
-    @files.delete_if {|file| file == "." || file == ".."}
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
   end
 
   def import
-    self.files.each do |file|
-      song = Song.new_by_filename(file)
-      Artist.all << song.artist unless Artist.all.include?(song.artist)
-    end
+    files.each {|song| Song.new_by_filename(song)}
   end
-
 
 end
