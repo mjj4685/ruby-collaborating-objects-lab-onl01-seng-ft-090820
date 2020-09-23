f456e3751ccf49cc351944c9064eae8bccc642a5
MJ Johnson <mjohnson4685@gmail.com>
2020-09-23 15:55:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,8 @@ class Song
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
     song = self.new(split_name[1])
-    artist = Artist.find_or_create_by_name(name)
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
   end
 
   def self.all
